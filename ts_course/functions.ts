27389c8e8d76e87a5783038e21bdd1317e64b1c5
Lauren <ls_garwood@yahoo.co.uk>
2022-07-12 18:56:26 +0100

    ts functions

--- ts_course/functions.ts
+++ ts_course/functions.ts
@@ -1,2 +1,60 @@
 //Intro
+function printOperations(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers!');
+  }
 
+  console.log(a + b, a / b);
+}
+
+// The function call below should print: 12 1 
+printOperations(6, 6);
+
+function exclaim(name, count) {
+  for (let i = 0; i < count; i += 1) {
+    console.log(`${name}!`);
+  }
+}
+
+// Exclaim 'Muriel!' six times
+exclaim('Muriel', 6);
+
+//-----------
+// Parameter types
+
+function triple(value: number) {
+  return value * 3;
+}
+
+function greetTripled(greeting: string, value: number) {
+  console.log(`${greeting}, ${triple(value)}!`);
+}
+
+greetTripled('Hiya', 5);
+
+//---------
+// Optional Parameters
+
+function proclaim(status?: string) {
+  console.log(`I'm ${status || 'not ready...'}`);
+}
+
+proclaim();
+proclaim('ready?');
+proclaim('ready!');
+
+//----------
+// default parameters
+
+function proclaimOne(status = 'not ready...', repeat = 1) {
+  for (let i = 0; i < repeat; i += 1) {
+    console.log(`I'm ${status}`);
+  }
+}
+
+proclaimOne();
+proclaimOne('ready?');
+proclaimOne('ready!', 3);
+
+//-----------
+// 
