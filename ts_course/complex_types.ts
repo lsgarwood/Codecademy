c806ce5884e91b2762c597fa3d6d96491df15f31
Lauren <ls_garwood@yahoo.co.uk>
2022-07-14 10:54:41 +0100

    ts function types

--- ts_course/complex_types.ts
+++ ts_course/complex_types.ts
@@ -169,4 +169,97 @@ function mathTutor(operationCallback: OperatorFunction) {
 type OperatorFunction = (arg0: number, arg1: number) => number;
 
 //---------------
-// 
+// Generic types
+
+type Human = {name: string, job: string};
+type Dog = {name: string, tailWagSpeed: number};
+
+type Family<T> = {
+  parents: [T, T], mate: T, children: T[]
+};
+//Do not change the code above this line.
+
+//Provide type annotations for the variables below:
+let theFamily: Family<number> = {
+  parents: [3, 4], mate: 9, children: [5, 30, 121]
+};
+
+let someFamily: Family<boolean> = {
+  parents: [true, true], mate: false, 
+  children: [false, false, true, true]
+};
+
+let aFamily: Family<Human> = {
+  parents: [
+    {name: 'Mom', job: 'software engineer'},
+    {name: 'Dad', job: 'coding engineer'}
+  ],
+  mate: {name: 'Matesky', job: 'engineering coder'},
+  children: [{name: 'Babesky', job: 'none'}]
+};
+
+let anotherFamily: Family<Dog> = {
+  parents: [
+    {name: 'Momo', tailWagSpeed: 3},
+    {name: 'Dado', tailWagSpeed: 100}
+  ],
+  mate: {name: 'Cheems', tailWagSpeed: 7},
+  children: [
+    {name: 'Puppin', tailWagSpeed: 0.001},
+    {name: 'Puppenaut', tailWagSpeed: 0.0001},
+    {name: 'Puppenator', tailWagSpeed: 0.01}
+  ]
+};
+
+//
+
+// This is a generic type alias
+type Collection<G> = {
+  name: string,
+  quantity: number,
+  content: G[]
+};
+ 
+let bookCollection: Collection<string> = {
+  name: 'Nursery Books',
+  quantity: 3,
+  content: ['Goodnight Moon', 'Humpty Dumpty', 'Green Eggs & Ham']
+};
+ 
+let primeNumberCollection: Collection<number> = {
+  name: 'First 5 Prime Numbers',
+  quantity: 5,
+  content: [2, 3, 5, 7, 11]
+};
+
+//-----------
+// Generic function
+
+// This is a generic function type alias
+function findMiddleMember<M>(members: M[]): M {
+  return members[Math.floor(members.length/2)];
+}
+ 
+// Call function for an array of strings
+console.log(findMiddleMember<string>(['I', 'am', 'very', 'happy'])); // Prints "very"
+ 
+// Call function for an array of numbers
+console.log(findMiddleMember<number>([210, 369, 102]));     // Prints 369
+
+// exercise
+
+function getFilledArray<T>(value: T, n: number): T[] {
+  return Array(n).fill(value);
+}
+
+let stringArray: string[];
+let numberArray: number[];
+let personArray: {name: string, age: number}[];
+let coordinateArray: [number, number][];
+
+// Write your code below:
+stringArray = getFilledArray<string>('hi', 6);
+numberArray = getFilledArray<number>(9, 6);
+personArray = getFilledArray<{name: string, age: number}>({name: 'J. Dean', age: 24}, 6);
+coordinateArray = getFilledArray<[number, number]>([3, 4], 6);
+
