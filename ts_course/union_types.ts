06ce74db17f2156626ff93c88accc4a493c2e69d
Lauren <ls_garwood@yahoo.co.uk>
2022-07-15 12:56:04 +0100

    ts union literal types

--- ts_course/union_types.ts
+++ ts_course/union_types.ts
@@ -52,7 +52,7 @@ formatValue('Hiya');
 formatValue(42);
 
 //------------------
-// Inferred Unioin Return Types
+// Inferred Union Return Types
 
 const popStack = (stack: string[]) => {
   if (stack.length) {
@@ -70,3 +70,145 @@ console.log(item);  // Prints null
 
 //
 
+type User = {
+  id: number;
+  username: string;
+};
+
+function createUser() {
+  const randomChance = Math.random() >= 0.5;
+
+  if (randomChance) {
+    return { id: 1, username: 'nikko' };
+  } else {
+    return 'Could not create a user.';
+  }
+}
+
+const userData: string | User = createUser() 
+
+//-----------------
+// Unions and Arrays
+
+function formatListings(listings: (string |number)[]) {
+  return listings.map((listing) => {
+    if (typeof listing === 'string') {
+      return listing.toUpperCase();
+    }
+
+    if (typeof listing === 'number') {
+      return `$${listing.toLocaleString()}`;
+    }
+  });
+}
+
+const result = formatListings([
+  '123 Main St',
+  226800,
+  '580 Broadway Apt 4a',
+  337900,
+]);
+
+console.log(result);
+
+//
+
+const removeDashes = (id: string | number) => {
+  if (typeof id === 'string') {
+    id = id.split('-').join('');
+    return parseInt(id);
+  } else {
+    return id;
+  }
+} 
+// This is a union of array types
+let ids: (number | string)[] = ['93-235-66', '89-528-92'];
+let newIds: (number | string)[] = [];
+for (let i=0; i < ids.length; i++) {
+  newIds[i] = removeDashes(ids[i]); // Convert string id to number id
+}
+console.log(newIds);  // Prints [9323566, 8952892]
+
+//-----------------
+// Union Type Common Property Access
+
+let element: string | number[] = 'Codecademy';
+// The .length property is common for string and array
+console.log(element.length);      // Prints 10
+// The .match method only works for a string type
+console.log(element.match('my')); // Prints ["my"]
+ 
+element = [3, 5, 1];
+// The length property is common for string and array
+console.log(element.length);      // Prints 3
+// The .match method will not work for an array type
+// console.log(element.match(5));  // Error: Property 'match' does not exist on type 'number[]'.
+
+//---------------
+// Common key value pairs
+
+type Like = {
+  username: string;
+  displayName: string;
+};
+
+type Share = {
+  username: string;
+  displayName: string;
+};
+
+function getFriendNameFromEvent(event: Like | Share) {
+  return event.displayName || event.username;
+}
+
+const newEvent = {
+  username: 'vkrauss',
+  displayName: 'Veronica Krauss',
+};
+
+const friendName = getFriendNameFromEvent(newEvent);
+
+console.log(`You have an update from ${friendName}.`);
+
+//-----------------
+// Unions with literal types
+
+// This is a union of string literal types
+type RPS = 'rock' | 'paper' | 'scissors' ;
+const play = (choice: RPS): void => {
+  console.log('You: ', choice);
+  let result: string = '';
+  switch (choice) {
+    case 'rock':
+      result = 'paper';
+      break;
+    case 'paper':
+      result = 'scissors';
+      break;
+    case 'scissors':
+      result = 'rock';
+      break;
+  }
+  console.log('Me: ', result);
+}
+const number = Math.floor(Math.random()*3);
+let choicesOne: [RPS, RPS, RPS] = ['rock', 'paper', 'scissors'];
+play(choicesOne[number]); 
+
+// exercise
+ 
+type Status = 'idle' | 'downloading' | 'complete';
+
+function downloadStatus(status: Status) {
+  if(status === 'idle') {
+    console.log('Your download is complete!');
+  }
+  if(status === 'downloading') {
+    console.log('Downloading...');
+  }
+  if(status === 'complete') {
+    console.log('Your download is complete!');
+  }
+}
+
+downloadStatus('idle');
