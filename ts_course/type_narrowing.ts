ca8384dbb0fc2e8bcdcf3a57e710c96a83e1e47c
Lauren <ls_garwood@yahoo.co.uk>
2022-08-09 21:55:20 +0100

    type guards

--- ts_course/type_narrowing.ts
+++ ts_course/type_narrowing.ts
@@ -1,2 +1,14 @@
 // type guards
 
+function formatStatistic(stat: string | number) {
+  if(typeof stat === 'number') {
+    return stat.toFixed(2);
+  } 
+  if(typeof stat === 'string') {
+    return stat.toUpperCase()
+  }
+}
+
+console.log(formatStatistic('Win'));
+console.log(formatStatistic(0.364));
+
