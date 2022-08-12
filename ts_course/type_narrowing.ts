da8be0553719594586d27bc2b1c8ab1ff1da3e3c
Lauren <ls_garwood@yahoo.co.uk>
2022-08-12 20:13:10 +0100

    in with type gurards

--- ts_course/type_narrowing.ts
+++ ts_course/type_narrowing.ts
@@ -12,3 +12,40 @@ function formatStatistic(stat: string | number) {
 console.log(formatStatistic('Win'));
 console.log(formatStatistic(0.364));
 
+// Using in with type guards
+
+type Cat = {
+  name: string;
+  run: () => string;
+}
+
+type Fish = {
+  name: string;
+  swim: () => string;
+}
+
+const siameseCat = { 
+  name: 'Proxie', 
+  run: () => 'pitter pat'
+}
+
+const bettaFish = { 
+  name: 'Neptune', 
+  swim: () => 'bubble blub'
+}
+
+function move(pet: Cat | Fish) {
+  if ('run' in pet) {
+    return pet.run();
+  }
+  if ('swim' in pet) {
+    return pet.swim();
+  }
+}
+
+console.log(move(siameseCat))
+
+// Narrowing with else
+
+
+
