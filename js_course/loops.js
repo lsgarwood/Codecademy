120565bd80124c5b1b1609e82f8547fc0d23074a
Lauren Garwood <ls_garwood@yahoo.co.uk>
2023-05-14 10:41:16 +0100

    loops - whale speak exercise

--- js_course/loops.js
+++ js_course/loops.js
@@ -84,3 +84,38 @@ for (let i = 0; i < rapperArray.length; i++) {
 }
 
 console.log("And if you don't know, now you know.");
+
+// ---------------------
+// Whale Speak exercise
+
+const input = "turpentine and turtles";
+
+const vowels = ["a", "e", "i", "o", "u"];
+
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  // console.log("i is "+ i);
+  if (input[i] === "e") {
+    resultArray.push(input[i]);
+  }
+
+  if (input[i] === "u") {
+    resultArray.push(input[i]);
+  }
+
+  for (let x = 0; x < vowels.length; x++) {
+    // console.log("x is "+ x);
+    if (input[i] === vowels[x]) {
+      // console.log(input[i]);
+      resultArray.push(input[i]);
+    }
+  }
+}
+
+// console.log(resultArray);
+
+const resultString = resultArray.join("").toUpperCase();
+
+console.log("Input: " + input)
+console.log("In Whale: " + resultString);
